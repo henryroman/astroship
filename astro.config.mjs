18f082879387b73e1bd2bde691def4fbf55e8bb4
henryroman <162192380+henryroman@users.noreply.github.com>
2024-07-05 09:03:44 +0200

    more changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,24 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  base: "/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  pages: [
+    {
+      path: "/features1",
+      component: "src/components/featuresIntegration2.astro"
+    },
+    {
+      path: "/features2",
+      component: "src/components/featuresbi2.astro"
+    },
+    {
+      path: "/features3",
+      component: "src/components/featuresLearning2.astro"
+    },
+    {
+      path: "/features4",
+      component: "src/components/features3.astro"
+    },
+  ]
 });
